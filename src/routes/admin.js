85d660e2e8d8498742cc20677e13840631d9d08a
Franco <franco.villegas@alumnos.uv.cl>
2021-04-17 16:46:42 -0400

    Cambios - Franco
    Vistas admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,4 +5,16 @@ router.get('/admin', (req, res) => {
     res.render('admin.ejs');
 });
 
+
+router.get('/servicios', (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer', (err, customer) => {
+            res.render('admin-servicios', {
+                data: customer
+            });
+        });
+    });
+});
+
 module.exports = router;
