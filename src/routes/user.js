fec0edc816b20d19f1343cfc3f36919940af0365
Nicolás Veas <nicolas.veas@alumnos.uv.cl>
2021-04-19 20:57:13 -0400

    agregar servicio-emprendedor

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,4 +6,30 @@ router.get('/usuario', (req, res) => {
     res.render('user.ejs');
 });
 
+// Agregar servicio emprendedor
+router.post('/agregar-servicio-emprendedor', (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next();
+    }
+        res.redirect('/');
+    },(req,res) =>{
+
+    let data = Object.assign({},req.body);
+    let user = req.user.correo;
+    console.log(data);
+    console.log(user);
+    conn.query('INSERT INTO servicio_emprendedor set ? ', {
+        titulo: data.titulo,
+        correo: user,
+        descripcion: data.descripcion,
+        ubicacion: data.ubicacion,
+        contacto_tel: data.contacto_tel,
+        contacto_correo: data.contacto_correo,
+        redsocial: data.redsocial,
+        tipo: data.tipo
+    }, (err, customer) => {
+        res.redirect('/usuario');
+    });
+});
+
 module.exports = router;
