2338a6c1311b3818d70f13f69da9c5186bdc0b77
Franco <franco.villegas@alumnos.uv.cl>
2021-04-20 23:14:22 -0400

    Cambios - Franco
    Iconos para las puntos de los mapas + arreglo de js + agregando de algunos puntos.

--- src/static/js/navegador.js
+++ src/static/js/navegador.js
@@ -6,15 +6,3 @@ setInterval(function () {
         counter = 1;
     }
 }, 5000);
-
-// Mapa
-// -33.59829709005887, -71.60798902585982 Coordenadas San Antonio
-var map = L.map('mapid').setView([-33.59829709005887, -71.60798902585982], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([-33.59829709005887, -71.60798902585982]).addTo(map)
-    .bindPopup('Centro de San Antonio')
-    .openPopup();
