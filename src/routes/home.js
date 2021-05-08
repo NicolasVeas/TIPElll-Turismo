de31bdd04770fed22ec8924bc8fed05d6c6ff5b8
Franco <franco.villegas@alumnos.uv.cl>
2021-05-07 20:36:02 -0400

    Cambios - Franco
    Mapa funcional de atractivos y servicios.

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,33 +2,33 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../database');
 
-router.get('/test', (req,res,next) => {
-    conn.query('SELECT * FROM atractivo_admin', (err, atractivos) => {
-        console.log(atractivos[0].geo_local);
+router.get('/', (req, res) => {
+    res.render('home.ejs', {
+        usuario: req.user
+    });
+});
+
+router.get('/servicios-turisticos', (req, res) => {
+    conn.query('SELECT * FROM servicio_admin', (err, atractivos) => {
         if (err) {
             res.json(err);
         }
-        res.render('x.ejs', {
+        res.render('servicios.ejs', {
+            usuario: req.user,
             data: atractivos
         });
     });
 });
 
-router.get('/', (req, res) => {
-    res.render('home.ejs', {
-        data: req.user
-    });
-});
-
-router.get('/servicios-turisticos', (req, res) => {
-    res.render('servicios.ejs', {
-        data: req.user
-    });
-});
-
-router.get('/atractivos-turisticos', (req, res) => {
-    res.render('atractivos.ejs', {
-        data: req.user
+router.get('/atractivos-turisticos', (req,res,next) => {
+    conn.query('SELECT * FROM atractivo_admin', (err, atractivos) => {
+        if (err) {
+            res.json(err);
+        }
+        res.render('atractivos.ejs', {
+            usuario: req.user,
+            data: atractivos
+        });
     });
 });
 
