56bfa74e435870eabc388df1f674c7bdd5f11340
Franco <franco.villegas@alumnos.uv.cl>
2021-05-17 21:04:35 -0400

    Cambios - Franco

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,19 +4,6 @@ const conn = require('../database');
 
 // Pruebas
 
-router.get('/x', (req, res) => {
-    conn.query('SELECT * FROM atractivo_admin', (err, atractivos) => {
-        if (err) {
-            res.json(err);
-        }
-        res.render('individual.ejs', {
-            usuario: req.user,
-            data: atractivos
-        });
-    });
-});
-
-
 router.get('/', (req, res) => {
     res.render('home.ejs', {
         usuario: req.user
@@ -24,25 +11,38 @@ router.get('/', (req, res) => {
 });
 
 router.get('/servicios-turisticos', (req, res) => {
-    conn.query('SELECT * FROM servicio_admin', (err, atractivos) => {
+    conn.query('SELECT * FROM servicio_admin', (err, data) => {
         if (err) {
             res.json(err);
         }
         res.render('servicios.ejs', {
             usuario: req.user,
-            data: atractivos
+            data: data
         });
     });
 });
 
 router.get('/atractivos-turisticos', (req,res,next) => {
-    conn.query('SELECT * FROM atractivo_admin', (err, atractivos) => {
+    conn.query('SELECT * FROM atractivo_admin', (err, data) => {
         if (err) {
             res.json(err);
         }
         res.render('atractivos.ejs', {
             usuario: req.user,
-            data: atractivos
+            data: data
+        });
+    });
+});
+
+router.get('/atractivo/:id_atractivo', (req, res) => {
+    const { id_atractivo } = req.params;
+    conn.query('SELECT * FROM atractivo_admin WHERE id_atractivo = ?', [id_atractivo], (err, data) => {
+        if (err) {
+            res.json(err);
+        }
+        res.render('individual.ejs', {
+            usuario: req.user,
+            data: data
         });
     });
 });
